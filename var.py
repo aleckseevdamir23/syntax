ac8a957c784b9b900932536a6d4a312df61546a1
aleckseevdamir23 <aleckseevdamir23@gmail.com>
2023-11-01 20:38:48 +0900

    добавили 2_var

---
+++ var.py
@@ -0,0 +1,25 @@
+var_1 = 10 
+number_of_apples = 100
+myNumVar = 20
+CONST_VAR = 1000
+# print (var_1)
+# print (myNumVar)
+
+# print(myNumVar, CONST_VAR, number_of_apples)
+
+# встроенная функция ввода из терминала
+# in_1 = input("Введите число: ")
+
+# print(in_1)
+
+# небольшой калькулятоsр
+ 
+in_1 = input("Введите 1 число:")
+in_2 = input("Введите 2 число:")
+# конкатенация (обьеденение) строк
+# result = in_1 + in_2
+
+
+result = in_1 + in_2
+
+print(result)
